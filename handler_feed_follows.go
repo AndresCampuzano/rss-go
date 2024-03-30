4d067d884b71b189a5edfc9b917821fe427efe31
Andres Campuzano Garzon <campuxano88@gmail.com>
2024-03-30 14:04:35 -0500

    implement FeedFollow functionality and routes

---
+++ handler_feed_follows.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/AndresCampuzano/rss-go/internal/database"
+	"github.com/google/uuid"
+	"net/http"
+	"time"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+
+	feedFollow, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    params.FeedID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error creating feed follow: %v", err))
+		return
+	}
+	responseWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
+}
